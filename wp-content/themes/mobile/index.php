82fa34aba720e08277a074a6d1df2494ddf7762e
Ivan Avraamov <ivan.avraamov@gmail.com>
2018-09-10 18:47:50 +0300

    mobile-1

--- wp-content/themes/mobile/index.php
+++ wp-content/themes/mobile/index.php
@@ -5,7 +5,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<title>WizardMachine(Mobile)</title>
 		<link rel=stylesheet href=<?php echo get_template_directory_uri(); ?>/css/bootstrap.min.css type=text/css>
-		<link rel=stylesheet href=<?php echo get_template_directory_uri(); ?>/css/style.css type=text/css>
+		<link rel=stylesheet href=<?php echo get_template_directory_uri(); ?>/style.css type=text/css>
 	</head>
 	<body>
 		<div class="container-fluid">
