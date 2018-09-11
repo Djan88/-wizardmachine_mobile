fb223c526326d2eef4270142ea62abe530a0acdb
Ivan Avraamov <ivan.avraamov@gmail.com>
2018-09-12 01:22:16 +0300

    mobile-68

--- wp-content/themes/mobile/index.php
+++ wp-content/themes/mobile/index.php
@@ -27,7 +27,7 @@
 					<div class="step2">
 					<h3>Выделите область для обрезки</h3>
 					<img id="preview" alt="" />
-					<canvas id="preview-canvas" style="border: 3px red solid; position: absolute; visibility: hidden; left: -20000px"></canvas>
+					<!--<canvas id="preview-canvas" style="border: 3px red solid;/*position: absolute; visibility: hidden; /*left: -20000px*/"></canvas>-->
 					<div class="info hidden"><label>Размер файла</label> <input id="filesize" name="mci_filesize" type="text" />
 					<label>Тип</label> <input id="filetype" name="mci_filetype" type="text" />
 					<label>Разрешение изображения</label> <input id="filedim" name="mci_filedim" type="text" />
