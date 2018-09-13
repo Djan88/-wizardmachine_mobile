be7f67ca5d7c6a7127f88de96fe7acaf701bfa99
Ivan Avraamov <ivan.avraamov@gmail.com>
2018-09-13 22:57:56 +0300

    mobile-94

--- wp-content/themes/mobile/index.php
+++ wp-content/themes/mobile/index.php
@@ -42,18 +42,13 @@
 		<!-- what way -->
 		<div class="mobile_screen mobile_screen_what_way hidden">
 			<div class="row">
-				<div class="col">
-					<h4>Выберите режим работы</h4>
-					<div class="row">
-						<div class="col">
-							<button class="btn btn-primary text_center btn_choice_protocol">Выбрать протокол</button>
-						</div>
-						<div class="col">
-							<button class="btn btn-primary text_center btn_choice_manual">Ручная диагностика</button>
-						</div>
-					</div>
-				</div>
 				<div class="col protocols_info text_center">
+					<div class="col">
+						<button class="btn btn-primary text_center btn_choice_protocol">Выбрать протокол</button>
+					</div>
+					<div class="col">
+						<button class="btn btn-primary text_center btn_choice_manual">Ручная диагностика</button>
+					</div>
 					<img class="loaded_img" src="" alt="">
 				</div>
 			</div>
