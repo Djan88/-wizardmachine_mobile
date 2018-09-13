306241a6bad01006799bbb4f76c76f41422cab05
Ivan Avraamov <ivan.avraamov@gmail.com>
2018-09-13 22:12:59 +0300

    mobile-85

--- wp-content/themes/mobile/js/script.js
+++ wp-content/themes/mobile/js/script.js
@@ -34,7 +34,7 @@ jQuery(document).ready(function () {
 
 
   // If img already download
-  croppedImg = jQuery('.protocols_info').children()[0];
+  croppedImg = jQuery('.protocols').children()[0];
   if (croppedImg && croppedImg.hasAttribute('src')) {
     jQuery('.mobile_screen_load').addClass('hidden');
     jQuery('.mobile_screen_what_way').fadeIn(500);
