54cc6827e82a0a946c765a3bd04e825a11d32bc8
Ivan Avraamov <ivan.avraamov@gmail.com>
2018-09-19 22:51:43 +0300

    mobile-178

--- wp-content/themes/mobile/index.php
+++ wp-content/themes/mobile/index.php
@@ -162,13 +162,29 @@
 						<div class="col-12 text_center">
 							<div class="row">
 								<div class="col zone_holder">
-									<div class="zone draggable">D1</div>
+									<div class="zone draggable draggable_lovushka">D-</div>
+									<div class="zone draggable draggable_d2">D2</div>
+									<div class="zone draggable draggable_d3">D3</div>
+									<div class="zone draggable draggable_d4">D4</div>
+									<div class="zone draggable draggable_d5">D5</div>
+									<div class="zone draggable draggable_d6">D6</div>
 								</div>
 								<div class="col zone_holder">
-									<div class="zone draggable">V1</div>
+									<div class="zone draggable draggable_v0">V0</div>
+									<div class="zone draggable draggable_v1">V1</div>
+									<div class="zone draggable draggable_v2">V2</div>
+									<div class="zone draggable draggable_v3">V3</div>
+									<div class="zone draggable draggable_v4">V4</div>
+									<div class="zone draggable draggable_v5">V5</div>
+									<div class="zone draggable draggable_v-">V-</div>
 								</div>
 								<div class="col zone_holder">
-									<div class="zone draggable">S1</div>
+									<div class="zone draggable draggable_s2_up">S2</div>
+									<div class="zone draggable draggable_s2_down">S2</div>
+									<div class="zone draggable draggable_s3">S3</div>
+									<div class="zone draggable draggable_s4">S4</div>
+									<div class="zone draggable draggable_s5">S5</div>
+									<div class="zone draggable draggable_s6">S6</div>
 								</div>
 							</div>
 						</div>
