3f6acdcb504ac790402ecbd9c34d5cd85250249d
Ivan Avraamov <ivan.avraamov@gmail.com>
2018-09-21 15:24:27 +0300

    final_screen-38

--- wp-content/themes/mobile/js/script.js
+++ wp-content/themes/mobile/js/script.js
@@ -166,6 +166,22 @@ jQuery(document).ready(function () {
     jQuery('.zone').css('background', 'rgba(83,35,69, 0.4)');
   });
 
+
+  // render chart
+  var data = {
+    // A labels array that can contain any sort of values
+    labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
+    // Our series array that contains series objects or in this case series data arrays
+    series: [
+      [5, 2, 4, 2, 0]
+    ]
+  };
+
+  // Create a new line chart object where as first parameter we pass in a selector
+  // that is resolving to our chart container element. The Second parameter
+  // is the actual data object.
+  new Chartist.Line('.ct-chart', data);
+
 //CROPPING SCRIPT
     // convert bytes into friendly format
     function bytesToSize(bytes) {
