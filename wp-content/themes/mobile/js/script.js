c2b35c459ab62bbb22daf997c681610b482df960
Ivan Avraamov <ivan.avraamov@gmail.com>
2018-09-22 13:16:39 +0300

    charts-31

--- wp-content/themes/mobile/js/script.js
+++ wp-content/themes/mobile/js/script.js
@@ -175,6 +175,7 @@ jQuery(document).ready(function () {
 
   // render chart
   Chart.defaults.global.elements.line.borderWidth = 1;
+  Chart.defaults.global.defaultFontColor = "rgba(255,255,255,0)";
 
   var ctx = document.getElementById('myChart').getContext('2d');
   var chart = new Chart(ctx, {
