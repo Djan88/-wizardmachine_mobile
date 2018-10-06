2aaa0915ff0fb43973be688835a274ad91ac2253
Ivan Avraamov <ivan.avraamov@gmail.com>
2018-10-06 19:31:52 +0300

    protocols-8

--- wp-content/themes/mobile/js/protocols.js
+++ wp-content/themes/mobile/js/protocols.js
@@ -29,35 +29,38 @@ chain = function (callback) {
 
 v1 = function(){
   chain(function(next) {
+    // Фаза 1
     console.log('1');
-    // setTimeout(function() {
-    //     console.log('2');
-    //     next();
-    // }, 1000);
     phaseOne = setInterval(function(){
-      if (count_animation <= 16){
-        jQuery('.draggable_d2').css({
-          color: 'transparent',
-          borderColor: 'transparent',
-          background: '#fff url(/wp-content/themes/mobile/img/plod.png) 0 0/100% no-repeat',
-          opacity: 0.8,
-          transform: 'rotate(-'+count_animation+'deg) scale(2)'
-        });
+      if (count_animation <= 12){
+        if (count_animation <= 4) {
+          jQuery('.draggable_v1').css({
+            color: 'transparent',
+            background: '#fff url(/wp-content/themes/mobile/img/disfunction.jpg) 0 0/100% no-repeat',
+            opacity: 0.8
+          });
+        } else if (count_animation <= 8) {
+          jQuery('.draggable_v1').css({
+            background: '#fff url(/wp-content/themes/mobile/img/travma.jpg) 0 0/100% no-repeat'
+          });
+        } else if (count_animation <= 12) {
+          jQuery('.draggable_v1').css({
+            background: '#fff url(/wp-content/themes/mobile/img/povregdenie_demona.jpg) 0 0/100% no-repeat'
+          });
+        }
         count_animation += 1;
         console.log(count_animation);
       } else {
         clearInterval(phaseOne);
         count_animation = 1;
-        jQuery('.draggable_d2').css({
+        jQuery('.draggable_v1').css({
           color: '#FFF0C7',
-          borderColor: '#FFF0C7',
           background: 'rgba(83, 35, 69, 0.4)',
-          opacity: 1,
-          transform: 'scale(1)'
+          opacity: 1
         });
         next();
       }
-    }, 250);
+    }, 1000);
   }).then(function(next) {
       setTimeout(function() {
           console.log('3');
