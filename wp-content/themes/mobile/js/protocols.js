99866e6047a6ace3510c01c2d00310f16c1007df
Ivan Avraamov <ivan.avraamov@gmail.com>
2018-10-06 18:54:02 +0300

    protocols-6

--- wp-content/themes/mobile/js/protocols.js
+++ wp-content/themes/mobile/js/protocols.js
@@ -30,34 +30,32 @@ chain = function (callback) {
 v1 = function(){
   chain(function(next) {
     console.log('1');
-    // setTimeout(function() {
-    //     console.log('2');
-    //     next();
-    // }, 1000);
-    phaseOne = setInterval(function(){
-      if (count_animation <= 16){                                                                         //90
-        jQuery('.draggable_d2').css({
-          color: 'transparent',
-          borderColor: 'transparent',
-          background: '#fff url(/wp-content/themes/mobile/img/plod.png) 0 0/100% no-repeat',
-          opacity: 0.8,
-          transform: 'rotate(-'+count_animation+'deg) scale(2)'
-        });
-        count_animation += 1;
-        console.log(count_animation);
-      } else {
-        clearInterval(phaseOne);
-        count_animation = 1;
-        jQuery('.draggable_d2').css({
-          color: '#FFF0C7',
-          borderColor: '#FFF0C7',
-          background: 'rgba(83, 35, 69, 0.4)',
-          opacity: 1,
-          transform: 'scale(1)'
-        });
-        next();
-      }
-    }, 250);
+    setTimeout(function() {
+      phaseOne = setInterval(function(){
+        if (count_animation <= 16){                                                                         //90
+          jQuery('.draggable_d2').css({
+            color: 'transparent',
+            borderColor: 'transparent',
+            background: '#fff url(/wp-content/themes/mobile/img/plod.png) 0 0/100% no-repeat',
+            opacity: 0.8,
+            transform: 'rotate(-'+count_animation+'deg) scale(2)'
+          });
+          count_animation += 1;
+          console.log(count_animation);
+        } else {
+          clearInterval(phaseOne);
+          count_animation = 1;
+          jQuery('.draggable_d2').css({
+            color: '#FFF0C7',
+            borderColor: '#FFF0C7',
+            background: 'rgba(83, 35, 69, 0.4)',
+            opacity: 1,
+            transform: 'scale(1)'
+          });
+          next();
+        }
+      }, 250);
+    }, 0);
   }).then(function(next) {
       setTimeout(function() {
           console.log('3');
