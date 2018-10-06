91d7d25f85adc54076858159ad389464e25400b5
Ivan Avraamov <ivan.avraamov@gmail.com>
2018-10-06 19:37:23 +0300

    protocols-9

--- wp-content/themes/mobile/js/protocols.js
+++ wp-content/themes/mobile/js/protocols.js
@@ -29,13 +29,14 @@ chain = function (callback) {
 
 v1 = function(){
   chain(function(next) {
-    // Фаза 1
-    console.log('1');
+    // Фаза 1.1
+    count_animation = 0;
     phaseOne = setInterval(function(){
       if (count_animation <= 12){
         if (count_animation <= 4) {
           jQuery('.draggable_v1').css({
             color: 'transparent',
+            transform: 'scale(1.5)',
             background: '#fff url(/wp-content/themes/mobile/img/disfunction.jpg) 0 0/100% no-repeat',
             opacity: 0.8
           });
@@ -49,16 +50,81 @@ v1 = function(){
           });
         }
         count_animation += 1;
-        console.log(count_animation);
       } else {
         clearInterval(phaseOne);
         count_animation = 1;
         jQuery('.draggable_v1').css({
           color: '#FFF0C7',
+          transform: 'scale(1.5)',
           background: 'rgba(83, 35, 69, 0.4)',
           opacity: 1
         });
-        next();
+        // Фаза 1.2
+        count_animation = 0;
+        phaseOne = setInterval(function(){
+          if (count_animation <= 12){
+            if (count_animation <= 4) {
+              jQuery('.draggable_v5').css({
+                color: 'transparent',
+                transform: 'scale(1.5)',
+                background: '#fff url(/wp-content/themes/mobile/img/disfunction.jpg) 0 0/100% no-repeat',
+                opacity: 0.8
+              });
+            } else if (count_animation <= 8) {
+              jQuery('.draggable_v5').css({
+                background: '#fff url(/wp-content/themes/mobile/img/travma.jpg) 0 0/100% no-repeat'
+              });
+            } else if (count_animation <= 12) {
+              jQuery('.draggable_v5').css({
+                background: '#fff url(/wp-content/themes/mobile/img/povregdenie_demona.jpg) 0 0/100% no-repeat'
+              });
+            }
+            count_animation += 1;
+          } else {
+            clearInterval(phaseOne);
+            count_animation = 1;
+            jQuery('.draggable_v5').css({
+              color: '#FFF0C7',
+              transform: 'scale(1.5)',
+              background: 'rgba(83, 35, 69, 0.4)',
+              opacity: 1
+            });
+            count_animation = 0;
+            // Фаза 1.3
+            phaseOne = setInterval(function(){
+              if (count_animation <= 12){
+                if (count_animation <= 4) {
+                  jQuery('.draggable_v-').css({
+                    color: 'transparent',
+                    transform: 'scale(1.5)',
+                    background: '#fff url(/wp-content/themes/mobile/img/disfunction.jpg) 0 0/100% no-repeat',
+                    opacity: 0.8
+                  });
+                } else if (count_animation <= 8) {
+                  jQuery('.draggable_v-').css({
+                    background: '#fff url(/wp-content/themes/mobile/img/travma.jpg) 0 0/100% no-repeat'
+                  });
+                } else if (count_animation <= 12) {
+                  jQuery('.draggable_v-').css({
+                    background: '#fff url(/wp-content/themes/mobile/img/povregdenie_demona.jpg) 0 0/100% no-repeat'
+                  });
+                }
+                count_animation += 1;
+              } else {
+                clearInterval(phaseOne);
+                count_animation = 1;
+                jQuery('.draggable_v-').css({
+                  color: '#FFF0C7',
+                  transform: 'scale(1.5)',
+                  background: 'rgba(83, 35, 69, 0.4)',
+                  opacity: 1
+                });
+                count_animation = 0;
+                next();
+              }
+            }, 1000);
+          }
+        }, 1000);
       }
     }, 1000);
   }).then(function(next) {
