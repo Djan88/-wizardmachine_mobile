8531136262c388423bbcad9f9225f1e823a50183
Ivan Avraamov <ivan.avraamov@gmail.com>
2018-11-03 17:15:15 +0300

    zones-22

--- wp-content/themes/mobile/index.php
+++ wp-content/themes/mobile/index.php
@@ -173,7 +173,7 @@
 							<div class="row">
 								<div class="col zone_holder">
 									<div class="status status_percent_wrap hidden">
-										Выполнено: <span class="status_percent">0%</span>
+										<span class="status_percent">0%</span>
 									</div>
 									<div class="zone draggable draggable_lovushka"></div>
 									<div class="zone draggable draggable_d2">D2</div>
