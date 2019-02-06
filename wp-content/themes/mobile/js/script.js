6c6d3dd1ddb0b3e67170bdfeca5d3218a7ad481d
Ivan Avraamov <ivan.avraamov@gmail.com>
2019-02-06 12:39:59 +0300

    tarot-45

--- wp-content/themes/mobile/js/script.js
+++ wp-content/themes/mobile/js/script.js
@@ -60,6 +60,7 @@ jQuery(document).ready(function () {
       axis: "y",
       drag: function() {
           jQuery('.card_solis').css('bottom', jQuery('.card_devil').css('top'));
+          line_drawing();
         // dragger = jQuery('.draggable_y').css('top');
         // dragger = dragger.substr(0, dragger.length - 2);
         // draggerDate = new Date();
