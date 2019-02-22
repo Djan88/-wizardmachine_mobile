7197aacbb73382bfceebba2f8091c2df7b39903e
Ivan Avraamov <ivan.avraamov@gmail.com>
2019-02-22 12:46:03 +0300

    tarot-66

--- wp-content/themes/mobile/js/harvi.js
+++ wp-content/themes/mobile/js/harvi.js
@@ -1,4 +1,6 @@
 jQuery(document).ready(function () {
+    var zone_top,
+        card_semi_height = parseInt(jQuery('.card_solis').css('height')) / 2;
     var THarviMoon = new Object();
     THarviMoon=function(){
     this.Day=0;
@@ -39,5 +41,35 @@ jQuery(document).ready(function () {
     var hm = new THarviMoon();
     var moon_day = hm.NowMoonDay();
     //var moon_day=hm.GetMoonDay(22,1,2011);
+
+    if (moon_day == 1 || moon_day == 2 || moon_day == 29) {
+        zone_top = parseInt(jQuery('.draggable_v5_1').css('top') + 50);
+        jQuery('.card_ghost_devil').css('top', (zone_top - card_semi_height) +'px');
+    } else if (moon_day == 3 || moon_day == 25 || moon_day == 26 || moon_day == 27 || moon_day == 28) {
+
+    } else if (moon_day == 4 || moon_day == 24) {
+
+    } else if (moon_day == 5 || moon_day == 6 || moon_day == 23) {
+
+    } else if (moon_day == 7 || moon_day == 22) {
+
+    } else if (moon_day == 8 || moon_day == 21) {
+
+    } else if (moon_day == 9 || moon_day == 20) {
+
+    } else if (moon_day == 10 || moon_day == 19) {
+
+    } else if (moon_day == 11 || moon_day == 18) {
+        zone_top = parseInt(jQuery('.draggable_v5').css('top') + 50);
+        jQuery('.card_ghost_devil').css('top', (zone_top - card_semi_height) +'px');
+    } else if (moon_day == 12 || moon_day == 17) {
+
+    } else if (moon_day == 13 || moon_day == 16) {
+
+    } else if (moon_day == 14 || moon_day == 15) {
+
+    }
+
+
     console.log("Лунный день по ф. Харви на "+hm.Day+'.'+hm.Month+'.'+hm.Year+' - '+moon_day);
 });
