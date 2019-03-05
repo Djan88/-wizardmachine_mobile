027fae6ab1fe9dc07698019ec6c0ac8634fb3093
Ivan Avraamov <ivan.avraamov@gmail.com>
2019-03-05 22:32:29 +0300

    card_avatars-60

--- wp-content/themes/mobile/js/script.js
+++ wp-content/themes/mobile/js/script.js
@@ -44,10 +44,10 @@ jQuery(document).ready(function () {
     devil_x = jQuery('.card_devil').position().left + drag_zone_w + diag_zone_w;
     devil_y = jQuery('.card_devil').position().top + card_heigth;
 
-    jQuery('.line').attr('x1', solis_x);
-    jQuery('.line').attr('y1', solis_y-30);
-    jQuery('.line').attr('x2', devil_x);
-    jQuery('.line').attr('y2', devil_y+30);
+    jQuery('.line').attr('x1', solis_x - 30);
+    jQuery('.line').attr('y1', solis_y);
+    jQuery('.line').attr('x2', devil_x + 30);
+    jQuery('.line').attr('y2', devil_y);
   }
 
   //Dragging elems
