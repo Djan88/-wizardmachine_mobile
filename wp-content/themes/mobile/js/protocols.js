ae09d5fe23a434119f8a50ab191dfcf3fa27e2bf
Ivan Avraamov <ivan.avraamov@gmail.com>
2019-03-11 23:51:48 +0300

    cards-53

--- wp-content/themes/mobile/js/protocols.js
+++ wp-content/themes/mobile/js/protocols.js
@@ -214,6 +214,7 @@ jQuery(document).ready(function () {
 
   // choice card
   jQuery('.zone_choice').on('click', function(event) {
+    cur_cards.splice(cur_cards.indexOf(choiced_card), 1);
     cur_cards.push(choiced_card);
     jQuery('.card_codes_bottom').find(jQuery('[data-number =' + choiced_card + ']')).removeClass('hidden');
     console.log(cur_cards);
