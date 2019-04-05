2819176ae7ad3a4e4ef0a9c00aa5001104537f51
Ivan Avraamov <ivan.avraamov@gmail.com>
2019-04-05 15:14:26 +0300

    fixes-15

--- wp-content/themes/mobile/index.php
+++ wp-content/themes/mobile/index.php
@@ -86,7 +86,7 @@
 							</div>
 							<div class="col-1 text_center"></div>
 							<div class="col-1 col-md-3 col-lg-1 text_center"></div>
-							<div class="col-10 col-md-6 col-lg-4 text_center mode-item mode-item_manual">
+							<div class="col-10 col-md-6 col-lg-4 text_center mode-item mode-item_manual_">
 								<span class="mode-item_title">Альтернативный режим</span>
 								<div class="mode-item_img">
 									<i class="fas fa-fingerprint"></i>
