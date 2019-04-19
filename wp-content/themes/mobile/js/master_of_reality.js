e632a103c535ccf29124200ab6f5d2a40996a258
Ivan Avraamov <ivan.avraamov@gmail.com>
2019-04-19 13:50:12 +0300

    alternative-12

--- wp-content/themes/mobile/js/master_of_reality.js
+++ wp-content/themes/mobile/js/master_of_reality.js
@@ -54,6 +54,7 @@ jQuery(document).ready(function () {
 
 
   jQuery('.marakata_sim_prot').on('click', function(event) {
+    console.log('click');
     if (jQuery(this).hasClass('marakata_sim_from')) {
       jQuery(this).addClass('marakata_sim-active');
       if (counter <= 9) {
