a149d264fc459e28100214e1984955eb8a800c9c
Ivan Avraamov <ivan.avraamov@gmail.com>
2019-04-21 10:52:35 +0300

    alternative-54

--- wp-content/themes/mobile/header.php
+++ wp-content/themes/mobile/header.php
@@ -76,6 +76,21 @@
                 <span aria-hidden="true">&times;</span>
               </button>
               <div class="col header-title">История рецептов</div>
+              <div class="history_wrapper">
+                <div class="history_item row" data-item_num="0">
+                  <div class="history_item_date col-3">07/04/2019</div>
+                  <div class="history_item_code col-6">
+                    <div class="history_item_code_1">2</div>
+                    <div class="history_item_code_2">4</div>
+                    <div class="history_item_code_3">7</div>
+                    <div class="history_item_code_4">9</div>
+                    <div class="history_item_code_dot">.</div>
+                    <div class="history_item_code_5">3</div>
+                  </div>
+                  <div class="history_item_type col-3"><button class="remove_history_item"><i class="fas fa-backspace"></i></button></div>
+                  <div class="history_item_type hidden">pents</div>
+                </div>
+              </div>
             </div>
             <div class="zones_template">
               <img src="<?php echo get_template_directory_uri(); ?>/img/zones.png" alt="zones">
