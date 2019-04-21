cdc2f60313a1e0a0c7e13433955b7bef310566ce
Ivan Avraamov <ivan.avraamov@gmail.com>
2019-04-21 16:07:00 +0300

    alternative-91

--- wp-content/themes/mobile/js/master_of_reality.js
+++ wp-content/themes/mobile/js/master_of_reality.js
@@ -145,6 +145,7 @@ cur_date = function(){
             title:"Оцифровка завершена",
             text: "Запомнить рецепт?",
             type: "input",
+            position: 'top-end',
             inputPlaceholder: "Коротко озаглавьте рецепт.",
             showCancelButton: true,
             closeOnConfirm: false,
