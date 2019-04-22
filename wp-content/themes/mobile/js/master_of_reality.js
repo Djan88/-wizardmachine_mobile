2fa3ce43c5317b8d635cb40651b2b88e5a9dc944
Ivan Avraamov <ivan.avraamov@gmail.com>
2019-04-22 17:27:33 +0300

    alternative-168

--- wp-content/themes/mobile/js/master_of_reality.js
+++ wp-content/themes/mobile/js/master_of_reality.js
@@ -39,6 +39,11 @@ jQuery(document).ready(function () {
       history_update,
       cur_history_item,
       cur_history_images,
+      cur_history_images_one,
+      cur_history_images_two,
+      cur_history_images_three,
+      cur_history_images_four,
+      cur_history_images_five,
       swords = {
         0: 'url(/wp-content/themes/mobile/img/cards/tarot_numbers/Swords10.) top left/100%;',
         1: 'url(/wp-content/themes/mobile/img/cards/tarot_numbers/Swords1.) top left/100%;',
@@ -342,13 +347,18 @@ cur_date = function(){
     } else {
       cur_history_images = wands
     }
-    jQuery('.marakata_modal_sim-1').css('background', cur_history_images[cur_history_item[0]]);
-    jQuery('.marakata_modal_sim-2').css('background', cur_history_images[cur_history_item[1]]);
-    jQuery('.marakata_modal_sim-3').css('background', cur_history_images[cur_history_item[2]]);
-    jQuery('.marakata_modal_sim-4').css('background', cur_history_images[cur_history_item[3]]);
-    jQuery('.marakata_modal_sim-5').css('background', cur_history_images[cur_history_item[4]]);
+    cur_history_images_one = cur_history_item[0];
+    cur_history_images_two = cur_history_item[1];
+    cur_history_images_three = cur_history_item[2];
+    cur_history_images_four = cur_history_item[3];
+    cur_history_images_five = cur_history_item[4];
+    jQuery('.marakata_modal_sim-1').css('background', cur_history_images[cur_history_images_one]);
+    jQuery('.marakata_modal_sim-2').css('background', cur_history_images[cur_history_images_two]);
+    jQuery('.marakata_modal_sim-3').css('background', cur_history_images[cur_history_images_three]);
+    jQuery('.marakata_modal_sim-4').css('background', cur_history_images[cur_history_images_four]);
+    jQuery('.marakata_modal_sim-5').css('background', cur_history_images[cur_history_images_five]);
   });
-  console.log(cur_history_images[cur_history_item[0]]);
+
 
 // TODO
 
