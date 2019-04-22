c789bc159e87d7e66b1be7627f13b182158e1f13
Ivan Avraamov <ivan.avraamov@gmail.com>
2019-04-22 16:57:26 +0300

    alternative-159

--- wp-content/themes/mobile/js/master_of_reality.js
+++ wp-content/themes/mobile/js/master_of_reality.js
@@ -347,7 +347,7 @@ cur_date = function(){
     jQuery('.marakata_modal_sim-3').css('background', 'url('+cur_history_images[2]+') top left/100% repeat-y;');
     jQuery('.marakata_modal_sim-4').css('background', 'url('+cur_history_images[3]+') top left/100% repeat-y;');
     jQuery('.marakata_modal_sim-5').css('background', 'url('+cur_history_images[4]+') top left/100% repeat-y;');
-    console.log(cur_history_item);
+    console.log(cur_history_images[0]);
   });
 
 
