fea695d7a51a632b556bde564c77388894c6db62
Ivan Avraamov <ivan.avraamov@gmail.com>
2019-04-22 17:35:24 +0300

    alternative-174

--- wp-content/themes/mobile/footer.php
+++ wp-content/themes/mobile/footer.php
@@ -98,7 +98,7 @@
       <div class="modal-dialog" role="document">
         <div class="modal-content">
           <div class="modal-header">
-            <h5 class="modal-title" id="history_item_modal_title">Modal title</h5>
+            <h5 class="modal-title" id="history_item_modal_title">Рецепт: "<span class="history_item_modal_sub_title"></span>"</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
