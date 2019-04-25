323eb77c35d132ba5bbaed17feaef94c911ecbd8
Ivan Avraamov <ivan.avraamov@gmail.com>
2019-04-25 12:05:53 +0300

    canvas-43

--- wp-content/themes/mobile/js/script.js
+++ wp-content/themes/mobile/js/script.js
@@ -328,7 +328,8 @@ jQuery(document).ready(function () {
     jQuery('.mobile_screen_protocols').fadeIn(500);
     jQuery('.loaded_img').attr('src', jQuery('.cropped_img').attr('src'));
     jQuery('.btn-back').removeClass('hidden');
-    jQuery('.btn-to_img').removeClass('hidden');
+    jQuery('.btn-to_img, btn-paused, .btn-to_protocols, .btn-played, .btn-to_manual').addClass('hidden');
+    jQuery('.btn-to_mode').removeClass('hidden');
     jQuery('.header-title').text('Выберите режим');
   }
 
