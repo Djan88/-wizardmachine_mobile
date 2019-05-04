a3f3dafececc618106db99ab602522e4b81557c4
Ivan Avraamov <ivan.avraamov@gmail.com>
2019-05-04 19:46:44 +0300

    elems-11

--- wp-content/themes/mobile/js/proelements.js
+++ wp-content/themes/mobile/js/proelements.js
@@ -58,10 +58,10 @@ jQuery(document).ready(function () {
       if (count_animation == 0){
         jQuery('.elems_card_1').fadeIn(500);
         jQuery('.elems_card_1').css('left', zone_left - 78 +'px');
-        jQuery('.elems_card_1').css('top', zone_top - 150 +'px');
+        jQuery('.elems_card_1').css('top', zone_top - 140 +'px');
       } else if (count_animation == 2) {
         jQuery('.elems_card_2').fadeIn(500);
-        jQuery('.elems_card_2').css('left', zone_left - 150 +'px');
+        jQuery('.elems_card_2').css('left', zone_left - 140 +'px');
         jQuery('.elems_card_2').css('top', zone_top - 78 +'px');
       } else if (count_animation == 4) {
 
