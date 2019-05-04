44a52fc7b6ed243cbfc2402f1b81ea36b0f4be20
Ivan Avraamov <ivan.avraamov@gmail.com>
2019-05-04 16:47:13 +0300

    info-52

--- wp-content/themes/mobile/footer.php
+++ wp-content/themes/mobile/footer.php
@@ -191,7 +191,7 @@
       </div>
     </div>
     <div class="modal" id="elem_mode" tabindex="-1" role="dialog">
-      <div class="modal-dialog modal-sm" role="document">
+      <div class="modal-dialog" role="document">
         <div class="modal-content">
           <div class="modal-header">
             <h5 class="modal-title">Активирован первоэлемент</h5>
@@ -200,8 +200,10 @@
             </button>
           </div>
           <div class="modal-body mode_content">
-            Первоэлемент: <span class="elem_mode_title"></span><br>
-            Карты: <span class="elem_mode_card"></span>
+            <p>
+              Первоэлемент: <span class="elem_mode_title"></span><br>
+              Карты: <span class="elem_mode_card"></span>
+            </p>
             <div class="how_to_elem">
               <p>Переместите зону как показано ниже и запустите выполнение программы.</p>
               <img src="<?php echo get_template_directory_uri(); ?>/img/hand_alt.gif" alt="">
