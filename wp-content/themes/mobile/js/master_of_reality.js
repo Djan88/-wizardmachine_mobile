d455319f5d923e633c3bee3d86accecf68132c18
Ivan Avraamov <ivan.avraamov@gmail.com>
2019-05-05 12:41:20 +0300

    manual-4

--- wp-content/themes/mobile/js/master_of_reality.js
+++ wp-content/themes/mobile/js/master_of_reality.js
@@ -340,6 +340,16 @@ cur_date = function(){
   });
 
 
+  // hide blocks history / manual when click on background
+
+  function hidePopUps(e) {
+      if(!e.target.matches('.history, .history *')) {
+          jQuery('.history').removeClass('history_visible');
+      }
+  };
+
+  window.addEventListener('click', hidePopUps);
+
   // save history
   jQuery('.save_history').on('click', function(event) {
     swal({
