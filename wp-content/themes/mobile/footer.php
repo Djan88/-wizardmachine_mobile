bc2a19dd73aa94f548c872c4e6e5907db5844743
Ivan Avraamov <ivan.avraamov@gmail.com>
2019-05-05 11:52:01 +0300

    elems-31

--- wp-content/themes/mobile/footer.php
+++ wp-content/themes/mobile/footer.php
@@ -217,5 +217,23 @@
         </div>
       </div>
     </div>
+    <div class="modal" id="card_docs" tabindex="-1" role="dialog">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">Свойства карт</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body mode_content">
+            
+          </div>
+          <div class="modal-footer card_modal_footer">
+            <button type="button" class="btn btn-primary" data-dismiss="modal" data-zone="">Закрыть</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </body>
 </html>
