4f3e0f33291f78d051dc67749331648fca2a579b
Ivan Avraamov <ivan.avraamov@gmail.com>
2019-05-16 14:16:02 +0300

    manual-76

--- wp-content/themes/mobile/js/proelements.js
+++ wp-content/themes/mobile/js/proelements.js
@@ -40,121 +40,119 @@ hide_cards = function(){
 
 // chose element
   jQuery('.elements_item').on('click', function(event) {
-    if (jQuery('.hand_wrap').hasClass('hand_wrap_disabled')) {
-
-    } else {
-      jQuery('.btn_start_elems').removeClass('hidden');
-      end_elem_prot();
-      jQuery('.hand_wrap').addClass('hand_wrap_disabled');
-      jQuery('.elements_item').removeAttr('data-toggle');
-      jQuery('.header-title').text('Переместите зону на ладонь');
-      hide_cards();
-      jQuery(this).addClass('active');
-      jQuery('.hand_wrap').fadeIn(500);
-      card_wrapper_width = zone_top = parseInt(jQuery('.hand_wrap').css('width'));
-      jQuery('.elems_card').css('left', card_wrapper_width / 2 - 110+'px');
-      jQuery('.elems_card_2').css('left', card_wrapper_width / 2 - 150+'px');
-      elem_type = jQuery(this).data('elem');
-      if (elem_type == 'air') {
-        jQuery('.elem_mode_title').text('Воздух');
-        jQuery('.elem_mode_card').text('Жезлы');
-        jQuery('.elem_card_sun').removeClass('hidden').css('display', 'block');
-        jQuery('.elem_card_queen_wands, .elem_card_king_wands, .elem_card_page_wands, .elem_card_knight_wands').removeClass('hidden').css('display', 'block');
-      } else if (elem_type == 'earth') {
-        jQuery('.elem_mode_title').text('Земля');
-        jQuery('.elem_mode_card').text('Мечи');
-        jQuery('.elem_card_queen_swords, .elem_card_king_swords, .elem_card_page_swords, .elem_card_knight_swords').removeClass('hidden').css('display', 'block');
-      } else if (elem_type == 'fire') {
-        jQuery('.elem_mode_title').text('Огонь');
-        jQuery('.elem_mode_card').text('Пентакли');
-        jQuery('.elem_card_queen_pents, .elem_card_king_pents, .elem_card_page_pents, .elem_card_knight_pents').removeClass('hidden').css('display', 'block');
-      } else if (elem_type == 'water') {
-        jQuery('.elem_mode_title').text('Вода');
-        jQuery('.elem_mode_card').text('Кубки');
-        jQuery('.elem_card_queen_cups, .elem_card_king_cups, .elem_card_page_cups, .elem_card_knight_cups').removeClass('hidden').css('display', 'block');
-      }
+    jQuery('.btn_start_elems').removeClass('hidden');
+    end_elem_prot();
+    jQuery('.header-title').text('Переместите зону на ладонь');
+    hide_cards();
+    jQuery(this).addClass('active');
+    jQuery('.hand_wrap').fadeIn(500);
+    card_wrapper_width = zone_top = parseInt(jQuery('.hand_wrap').css('width'));
+    jQuery('.elems_card').css('left', card_wrapper_width / 2 - 110+'px');
+    jQuery('.elems_card_2').css('left', card_wrapper_width / 2 - 150+'px');
+    elem_type = jQuery(this).data('elem');
+    if (elem_type == 'air') {
+      jQuery('.elem_mode_title').text('Воздух');
+      jQuery('.elem_mode_card').text('Жезлы');
+      jQuery('.elem_card_sun').removeClass('hidden').css('display', 'block');
+      jQuery('.elem_card_queen_wands, .elem_card_king_wands, .elem_card_page_wands, .elem_card_knight_wands').removeClass('hidden').css('display', 'block');
+    } else if (elem_type == 'earth') {
+      jQuery('.elem_mode_title').text('Земля');
+      jQuery('.elem_mode_card').text('Мечи');
+      jQuery('.elem_card_queen_swords, .elem_card_king_swords, .elem_card_page_swords, .elem_card_knight_swords').removeClass('hidden').css('display', 'block');
+    } else if (elem_type == 'fire') {
+      jQuery('.elem_mode_title').text('Огонь');
+      jQuery('.elem_mode_card').text('Пентакли');
+      jQuery('.elem_card_queen_pents, .elem_card_king_pents, .elem_card_page_pents, .elem_card_knight_pents').removeClass('hidden').css('display', 'block');
+    } else if (elem_type == 'water') {
+      jQuery('.elem_mode_title').text('Вода');
+      jQuery('.elem_mode_card').text('Кубки');
+      jQuery('.elem_card_queen_cups, .elem_card_king_cups, .elem_card_page_cups, .elem_card_knight_cups').removeClass('hidden').css('display', 'block');
     }
   });
 
 
 // start_protocol
  jQuery('.btn_start_elems').on('click', function(event) {
-  count_animation = 0;
-  stop_status = 0;
-  zone_left = parseInt(jQuery('.zone_elem').css('left'));
-  zone_top = parseInt(jQuery('.zone_elem').css('top'));
-  if (zone_top >= 100) {
-    jQuery(this).addClass('hidden');
-    jQuery('.zone_elem').addClass('hidden');
-    phaseOne = setInterval(function(){
-      console.log(count_animation);
-      if (count_animation == 0){
-        jQuery('.elems_card_1').fadeIn(500);
-        jQuery('.elems_card_1').css('left', zone_left - 78 +'px');
-        jQuery('.elems_card_1').css('top', zone_top - 140 +'px');
-      } else if (count_animation == 2) {
-        jQuery('.elems_card_2').fadeIn(500);
-        jQuery('.elems_card_2').css('left', zone_left - 140 +'px');
-        jQuery('.elems_card_2').css('top', zone_top - 58 +'px');
-      } else if (count_animation == 4) {
-        jQuery('.elems_card_3').fadeIn(500);
-        jQuery('.elems_card_3').css('left', zone_left - 78 +'px');
-        jQuery('.elems_card_3').css('top', zone_top - 190 +'px');
-      } else if (count_animation == 6) {
-        jQuery('.elems_card_4').fadeIn(500);
-        jQuery('.elems_card_4').css('left', zone_left - 78 +'px');
-        jQuery('.elems_card_4').css('top', zone_top - 230 +'px');
-      } else if (count_animation == 8) {
-        jQuery('.elems_card_5').fadeIn(500);
-        jQuery('.elems_card_5').css('left', zone_left - 78 +'px');
-        jQuery('.elems_card_5').css('top', zone_top - 270 +'px');
-      } else if (count_animation == 10)  {
-        jQuery('.btn_stop_elems').removeClass('hidden');
-      } else if (count_animation == 590)  {
-        count_animation = 0;
-        clearInterval(phaseOne);
-        swal({
-          title: "Протокол выполнен!",   
-          text: "Программа завершает свою работу",   
-          type: "info"
-        },
-        function(isConfirm){
-          if (isConfirm) {    
-            count_animation = 0;
-            setTimeout(end_elem_prot, 1000);
-          } 
-        });
-      }
-      if (stop_status == 1) {
-        stop_status = 0;
-        jQuery('.hand_wrap').removeClass('hand_wrap_disabled');
-        jQuery('.elements_item').attr('data-toggle', 'modal');
-        swal({
-          title: "Протокол выполнен!",   
-          text: "Программа завершает свою работу",   
-          type: "info"
-        },
-        function(isConfirm){
-          if (isConfirm) { 
-            clearInterval(phaseOne);   
-            count_animation = 0;
-            setTimeout(end_elem_prot, 1000);
-          } 
-        });
-      } else {
-        count_animation += 1;
-      }
-    }, 1000);
-    
+  if (jQuery('.hand_wrap').hasClass('hand_wrap_disabled')) {
+
   } else {
-    swal("Зона не на руке", "Перед стартом перенесите зону на руку", "info");
+    jQuery('.hand_wrap').addClass('hand_wrap_disabled');
+    jQuery('.elements_item').removeAttr('data-toggle');
+    count_animation = 0;
+    stop_status = 0;
+    zone_left = parseInt(jQuery('.zone_elem').css('left'));
+    zone_top = parseInt(jQuery('.zone_elem').css('top'));
+    if (zone_top >= 100) {
+      jQuery(this).addClass('hidden');
+      jQuery('.zone_elem').addClass('hidden');
+      phaseOne = setInterval(function(){
+        console.log(count_animation);
+        if (count_animation == 0){
+          jQuery('.elems_card_1').fadeIn(500);
+          jQuery('.elems_card_1').css('left', zone_left - 78 +'px');
+          jQuery('.elems_card_1').css('top', zone_top - 140 +'px');
+        } else if (count_animation == 2) {
+          jQuery('.elems_card_2').fadeIn(500);
+          jQuery('.elems_card_2').css('left', zone_left - 140 +'px');
+          jQuery('.elems_card_2').css('top', zone_top - 58 +'px');
+        } else if (count_animation == 4) {
+          jQuery('.elems_card_3').fadeIn(500);
+          jQuery('.elems_card_3').css('left', zone_left - 78 +'px');
+          jQuery('.elems_card_3').css('top', zone_top - 190 +'px');
+        } else if (count_animation == 6) {
+          jQuery('.elems_card_4').fadeIn(500);
+          jQuery('.elems_card_4').css('left', zone_left - 78 +'px');
+          jQuery('.elems_card_4').css('top', zone_top - 230 +'px');
+        } else if (count_animation == 8) {
+          jQuery('.elems_card_5').fadeIn(500);
+          jQuery('.elems_card_5').css('left', zone_left - 78 +'px');
+          jQuery('.elems_card_5').css('top', zone_top - 270 +'px');
+        } else if (count_animation == 10)  {
+          jQuery('.btn_stop_elems').removeClass('hidden');
+        } else if (count_animation == 590)  {
+          count_animation = 0;
+          clearInterval(phaseOne);
+          swal({
+            title: "Протокол выполнен!",   
+            text: "Программа завершает свою работу",   
+            type: "info"
+          },
+          function(isConfirm){
+            if (isConfirm) {    
+              count_animation = 0;
+              setTimeout(end_elem_prot, 1000);
+            } 
+          });
+        }
+        if (stop_status == 1) {
+          stop_status = 0;
+          jQuery('.hand_wrap').removeClass('hand_wrap_disabled');
+          jQuery('.elements_item').attr('data-toggle', 'modal');
+          swal({
+            title: "Протокол выполнен!",   
+            text: "Программа завершает свою работу",   
+            type: "info"
+          },
+          function(isConfirm){
+            if (isConfirm) { 
+              clearInterval(phaseOne);   
+              count_animation = 0;
+              setTimeout(end_elem_prot, 1000);
+            } 
+          });
+        } else {
+          count_animation += 1;
+        }
+      }, 1000);
+      
+    } else {
+      swal("Зона не на руке", "Перед стартом перенесите зону на руку", "info");
+    }
   }
-
+ });
 // stop protocol
-  jQuery('.btn_stop_elems').on('click', function(event) {
-    stop_status = 1;
-    jQuery('.elements_item').attr('data-toggle', 'modal');
-  });
-
+ jQuery('.btn_stop_elems').on('click', function(event) {
+   stop_status = 1;
+   jQuery('.elements_item').attr('data-toggle', 'modal');
  });
 });
