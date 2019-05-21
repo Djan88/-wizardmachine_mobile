ee9ce110e04b099d2c484e632b25e7c0b026f498
Ivan Avraamov <ivan.avraamov@gmail.com>
2019-05-21 13:54:11 +0300

    caliber-20

--- wp-content/themes/mobile/js/harvi.js
+++ wp-content/themes/mobile/js/harvi.js
@@ -51,7 +51,7 @@ jQuery(document).ready(function () {
         moon_day = moon_day-1;
     }
     // moon_day = moon_day - 1;
-    moon_day = 19;
+    moon_day = 20;
 
     localStorage.setItem('moon_day', moon_day);
     checkPoints = function(){
@@ -106,7 +106,8 @@ jQuery(document).ready(function () {
                 zone_top = parseInt(jQuery('.draggable_v5').css('top')) + 20;
                 zone_top_alt = parseInt(jQuery('.draggable_d2_1').css('top')) + 20;
             } else if (moon_day == 4 || moon_day == 20) {
-
+                zone_top = parseInt(jQuery('.draggable_v5').css('top')) + 20;
+                zone_top_alt = parseInt(jQuery('.draggable_v4-5').css('top')) + 20;
             } else if (moon_day == 5 || moon_day == 21) {
 
             } else if (moon_day == 6 || moon_day == 22) {
