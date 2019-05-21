cfe9a0d719913faecf38f946a68ff8aee49ff1c8
Ivan Avraamov <ivan.avraamov@gmail.com>
2019-05-21 13:31:02 +0300

    caliber-11

--- wp-content/themes/mobile/js/harvi.js
+++ wp-content/themes/mobile/js/harvi.js
@@ -51,7 +51,7 @@ jQuery(document).ready(function () {
         moon_day = moon_day-1;
     }
     // moon_day = moon_day - 1;
-    moon_day = 1;
+    moon_day = 18;
 
     localStorage.setItem('moon_day', moon_day);
     checkPoints = function(){
@@ -99,7 +99,32 @@ jQuery(document).ready(function () {
             if (moon_day == 1 || moon_day == 17) {
                 zone_top = parseInt(jQuery('.draggable_s5').css('top')) + 30;
                 zone_top_alt = parseInt(jQuery('.draggable_v1').css('top')) + 20;
-            } else if (moon_day == 3 || moon_day == 25 || moon_day == 26 || moon_day == 27 || moon_day == 28) {    
+            } else if (moon_day == 2 || moon_day == 18) {
+                zone_top = parseInt(jQuery('.draggable_s5').css('top')) - 30;
+                zone_top_alt = parseInt(jQuery('.draggable_d2').css('top'));
+            } else if (moon_day == 3 || moon_day == 19) {
+
+            } else if (moon_day == 4 || moon_day == 20) {
+
+            } else if (moon_day == 5 || moon_day == 21) {
+
+            } else if (moon_day == 6 || moon_day == 22) {
+
+            } else if (moon_day == 7 || moon_day == 23) {
+
+            } else if (moon_day == 8 || moon_day == 24) {
+
+            } else if (moon_day == 9 || moon_day == 25) {
+
+            } else if (moon_day == 10 || moon_day == 26) {
+
+            } else if (moon_day == 11 || moon_day == 27) {
+
+            } else if (moon_day == 12 || moon_day == 28) {
+
+            } else if (moon_day == 13 || moon_day == 29) {
+
+            } else if (moon_day == 14 || moon_day == 15 || moon_day == 16) {
 
             }
             // if (moon_day == 1 || moon_day == 2 || moon_day == 29) {
