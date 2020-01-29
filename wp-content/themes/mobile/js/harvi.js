e24cb56f7ef0f938c654c19351153c4f69442b3d
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-01-29 13:19:40 +0300

    tarot_fix-18

--- wp-content/themes/mobile/js/harvi.js
+++ wp-content/themes/mobile/js/harvi.js
@@ -106,7 +106,7 @@ jQuery(document).ready(function () {
                 zone_top = parseInt(jQuery('.draggable_v5').css('top')) + 20;
                 zone_top_alt = parseInt(jQuery('.draggable_d2_1').css('top')) + 20;
             } else if (moon_day == 10 || moon_day == 20) {
-                zone_top = parseInt(jQuery('.draggable_s5').css('top')) - 50;
+                zone_top = parseInt(jQuery('.draggable_s5').css('top')) + 50;
                 zone_top_alt = parseInt(jQuery('.draggable_d2').css('top')) - 20;
             } else if (moon_day == 9 || moon_day == 21) {
                 zone_top = parseInt(jQuery('.draggable_s5').css('top')) + 20;
