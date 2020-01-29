519cbfd93c85dd99096ea707f4d8e811767efadc
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-01-29 13:00:23 +0300

    tarot_fix-10

--- wp-content/themes/mobile/js/harvi.js
+++ wp-content/themes/mobile/js/harvi.js
@@ -51,7 +51,7 @@ jQuery(document).ready(function () {
         moon_day = moon_day-1;
     }
     // moon_day = moon_day - 1;
-    moon_day = 4;
+    moon_day = 5;
 
     localStorage.setItem('moon_day', moon_day);
     checkPoints = function(){
@@ -121,8 +121,8 @@ jQuery(document).ready(function () {
                 zone_top_alt = parseInt(jQuery('.draggable_v4-5').css('top')) + 10;
                 zone_top = parseInt(jQuery('.draggable_v3').css('top')) + 20;
             } else if (moon_day == 5 || moon_day == 25) {
-                zone_top_alt = parseInt(jQuery('.draggable_v5').css('top'));
-                zone_top = parseInt(jQuery('.draggable_d2_1').css('top')) - 20;
+                zone_top_alt = parseInt(jQuery('.draggable_d4').css('top')) + 20;
+                zone_top = parseInt(jQuery('.draggable_v3_4').css('top')) + 20;
             } else if (moon_day == 4 || moon_day == 26) {
                 zone_top_alt = parseInt(jQuery('.draggable_d5').css('top')) - 50;
                 zone_top = parseInt(jQuery('.draggable_v3').css('top')) + 20;
