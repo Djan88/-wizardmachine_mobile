691cc225b77965895fa60d3110f238a7ee6fa6a8
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-02-08 15:54:17 +0300

    moon day-6

--- wp-content/themes/mobile/js/harvi.js
+++ wp-content/themes/mobile/js/harvi.js
@@ -50,7 +50,7 @@ jQuery(document).ready(function () {
     // if (moon_day > 1 && moon_day != 30) {
     //     moon_day = moon_day-1;
     // }
-    moon_day = localStorage.getItem('moon_day_ls');
+    moon_day = localStorage.getItem('moon_day_ls').toFixed();
     // moon_day = 10;
 
     localStorage.setItem('moon_day', moon_day);
