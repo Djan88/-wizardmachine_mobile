613e583f26fc80fb86b9ab1bb2a2c8d15730f784
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-07-20 11:55:37 +0300

    elems_order-2

--- wp-content/themes/mobile/js/proelements.js
+++ wp-content/themes/mobile/js/proelements.js
@@ -97,11 +97,11 @@ hide_cards = function(){
       } else if (count_animation == 2) {
         jQuery('.elems_card_2').fadeIn(500);
         jQuery('.elems_card_2').css('left', zone_left - 78 +'px');
-        jQuery('.elems_card_2').css('top', zone_top - 230 +'px');
+        jQuery('.elems_card_2').css('top', zone_top - 190 +'px');
       } else if (count_animation == 4) {
         jQuery('.elems_card_3').fadeIn(500);
         jQuery('.elems_card_3').css('left', zone_left - 78 +'px');
-        jQuery('.elems_card_3').css('top', zone_top - 190 +'px');
+        jQuery('.elems_card_3').css('top', zone_top - 230 +'px');
       } else if (count_animation == 6) {
         jQuery('.elems_card_4').fadeIn(500);
         jQuery('.elems_card_4').css('left', zone_left - 140 +'px');
